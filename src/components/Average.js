520355e3f8c9ed01e1ce7d8e0d7fa38be9e0d6d9
mamin89 <mina_amin89@yahoo.com>
2022-12-05 14:18:12 -0500

    react

---
+++ src/components/Average.js
@@ -0,0 +1,10 @@
+function Average(){
+    return(
+        <div className='box average'>
+            <h3>Average Rating</h3>
+            <h3>4.6 ⭐</h3>
+         
+        </div>
+    )
+}
+export default Average
